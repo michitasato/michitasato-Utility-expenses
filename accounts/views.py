4d4c9ea9f88c10a8cf1106f65529bb2a2e1544cd
michitasato <nezimakidorizenmai@yahoo.co.jp>
2020-03-17 10:42:42 +0900

    add forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+from .forms import HelloForm
 
-def index(request):
-    params = {
-            'title':'Hello/Index',
-            'msg':'これは、サンプルで作ったページです。',
+
+class HelloView(TemplateView):
+
+    def __init__(self):
+        self.params = {
+            'title': ' Hellow',
+            'message': 'your data:',
+            'form': HelloForm()
         }
-    return render(request, 'accounts/index.html', params)
+
+    def get(self, request):
+        return render(request, 'accounts/index.html', self.params)
+
+    def post(self, request):
+        msg = 'あなたは <b>' + request.POST['name'] + \
+            '(' + request.POST['age'] + \
+            ')</b> さんですね。<br>メールアドレスは<b>' + request.POST['mail'] + \
+            '</b>ですね。'
+        self.params['message'] = msg
+        self.params['form'] = HelloForm(request.POST)
+        return render(request, 'accounts/index.html', self.params)
