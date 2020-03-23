435776700f26436d86ea39d7af06ba99b3e0f793
michitasato <nezimakidorizenmai@yahoo.co.jp>
2020-03-23 12:17:56 +0900

    add id form

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.generic import TemplateView
+from .models import Friend
 from .forms import HelloForm
 
+def index(request):
+    params = {
+        'title': 'Hello',
+        'message': 'all friends.',
+        'form': HelloForm(),
+        'data': [],
+    }
+    if (request.method == 'POST'):
+        num = request.POST['id']
+        item = Friend.objects.get(id=num)
+        params['data'] = [item]
+        params['form'] = HelloForm(request.POST)
+    else:
+        params['data'] = Friend.objects.all() 
+    return render(request, 'accounts/index.html', params)
+    
 
-class HelloView(TemplateView):
 
-    def __init__(self):
-        self.params = {
-            'title': ' Hellow',
-            'message': 'your data:',
-            'form': HelloForm()
-        }
-
-    def get(self, request):
-        return render(request, 'accounts/index.html', self.params)
-
-    def post(self, request):
-        msg = 'あなたは <b>' + request.POST['name'] + \
-            '(' + request.POST['age'] + \
-            ')</b> さんですね。<br>メールアドレスは<b>' + request.POST['mail'] + \
-            '</b>ですね。'
-        self.params['message'] = msg
-        self.params['form'] = HelloForm(request.POST)
-        return render(request, 'accounts/index.html', self.params)
